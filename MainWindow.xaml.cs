961d49575954787328c3ac17ea23e9666b8c7b68
ULYSSESS <ULYSSESS@DESKTOP-MK9933E>
2020-06-08 19:25:39 -0400

    Se agrego el comentario "Cambiar key por paso en el metodo guardar"

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -69,6 +69,7 @@ namespace Registro_de_Usuario
             this.DataContext = this.usuarios;
         }
 
+        /*Cambio key por paso*/
         private void guardarbotton_Click(object sender, RoutedEventArgs e)
         {
             if (!Validacion())
@@ -94,6 +95,7 @@ namespace Registro_de_Usuario
 
         private void Nuevobotton_Click(object sender, RoutedEventArgs e)
         {
+            Limpiar();
 
         }
 
